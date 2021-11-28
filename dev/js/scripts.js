1546ccbcd50c97675ab806050a16020f6a06551a
egoldberg30 <81780868+egoldberg30@users.noreply.github.com>
2021-11-27 23:13:54 -0500

    custom ease

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -1,9 +1,11 @@
 import { gsap } from "gsap";
-import {GSDevTools} from "gsap/GSDevTools";
-import {MorphSVGPlugin} from "gsap/MorphSVGPlugin";
+// import {GSDevTools} from "gsap/GSDevTools";
+// import {MorphSVGPlugin} from "gsap/MorphSVGPlugin";
 import {CustomEase} from "gsap/CustomEase";
 
-gsap.registerPlugin(GSDevTools, MorphSVGPlugin, CustomEase);
+// gsap.registerPlugin(GSDevTools, MorphSVGPlugin, CustomEase);
+// gsap.registerPlugin(MorphSVGPlugin, CustomEase);
+gsap.registerPlugin(CustomEase);
 const mainTL = gsap.timeline();
 
 
@@ -12,7 +14,10 @@ const mainTL = gsap.timeline();
 function LogoAnimation(){
     const tl = gsap.timeline();
 
-    tl.from ("#book",{duration: 1, y:"-=1000"});
+    tl.from ("#book",{duration: 1, y:"-=1000", ease: CustomEase.create("custom", "M0,0 C0.136,0.078 0.382,0.263 0.578,0.448 0.78,0.638 0.932,0.802 1,1 ")});
+
+
+    // CustomEase.create("custom", "M0,0 C0.136,0.078 0.382,0.263 0.578,0.448 0.78,0.638 0.932,0.802 1,1 ");
 
     return tl;
 }
@@ -23,4 +28,4 @@ function LogoAnimation(){
 
 mainTL.add(LogoAnimation());
 
-GSDevTools.create();
+// GSDevTools.create();
