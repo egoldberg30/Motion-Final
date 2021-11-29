6ed0f3576b090cf2baa316f28436c13857d0ad91
egoldberg30 <81780868+egoldberg30@users.noreply.github.com>
2021-11-29 10:42:02 -0500

    draw svg tweaks

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -2,8 +2,9 @@ import { gsap } from "gsap";
 import { GSDevTools } from "gsap/GSDevTools";
 import { MorphSVGPlugin } from "gsap/MorphSVGPlugin";
 import { CustomEase } from "gsap/CustomEase";
+import { DrawSVGPlugin } from "gsap/DrawSVGPlugin";
 
-gsap.registerPlugin(GSDevTools, MorphSVGPlugin, CustomEase);
+gsap.registerPlugin(GSDevTools, MorphSVGPlugin, CustomEase, DrawSVGPlugin);
 
 
 
@@ -15,23 +16,23 @@ function LogoAnimation(){
 
     tl.from ("#book",{duration: 1.3, y:"-=1000", ease: CustomEase.create("custom", "M0,0 C0.136,0.078 0.382,0.263 0.578,0.448 0.78,0.638 0.932,0.802 1,1 ")});
     tl.to("#book", {morphSVG:"#book_squashed", duration: .25})
-    .to("#book", {morphSVG:"#book", duration: .1})
+    .to("#book", {morphSVG:"#book", duration: .2})
     .to("#book_mask_1", {morphSVG:"#book_mask_2", duration: .01})
     .to("#book",{duration: .5, y:"-=200"}, "logo_fill")
 
-    .to("#Path_R_Page6", {drawSVG: "100%", duration: .5}, "logo_fill")
-    .to("#Path_R_Page5", {drawSVG: "100%", duration: .5}, "logo_fill")
-    .to("#Path_R_Page4", {drawSVG: "100%", duration: .5}, "logo_fill")
-    .to("#Path_R_Page3", {drawSVG: "100%", duration: .5}, "logo_fill")
-    .to("#Path_R_Page2", {drawSVG: "100%", duration: .5}, "logo_fill")
-    .to("#Path_R_Page1", {drawSVG: "100%", duration: .5}, "logo_fill")
+    tl.from("#Path_R_Page6", {drawSVG: "0%", transformOrigin:"0%", duration: .5}, "logo_fill")
+    .from("#Path_R_Page5", {drawSVG: "0%", duration: .5}, "logo_fill")
+    .from("#Path_R_Page4", {drawSVG: "0%", duration: .5}, "logo_fill")
+    .from("#Path_R_Page3", {drawSVG: "0%", duration: .5}, "logo_fill")
+    .from("#Path_R_Page2", {drawSVG: "0%", duration: .5}, "logo_fill")
+    .from("#Path_R_Page1", {drawSVG: "0%", duration: .5}, "logo_fill")
 
-    .to("#Path_L_Page6", {drawSVG: "100%", duration: .5}, "logo_fill")
-    .to("#Path_L_Page5", {drawSVG: "100%", duration: .5}, "logo_fill")
-    .to("#Path_L_Page4", {drawSVG: "100%", duration: .5}, "logo_fill")
-    .to("#Path_L_Page3", {drawSVG: "100%", duration: .5}, "logo_fill")
-    .to("#Path_L_Page2", {drawSVG: "100%", duration: .5}, "logo_fill")
-    .to("#Path_L_Page1", {drawSVG: "100%", duration: .5}, "logo_fill");
+    .from("#Path_L_Page6", {drawSVG: "0%", duration: .5}, "logo_fill")
+    .from("#Path_L_Page5", {drawSVG: "0%", duration: .5}, "logo_fill")
+    .from("#Path_L_Page4", {drawSVG: "0%", duration: .5}, "logo_fill")
+    .from("#Path_L_Page3", {drawSVG: "0%", duration: .5}, "logo_fill")
+    .from("#Path_L_Page2", {drawSVG: "0%", duration: .5}, "logo_fill")
+    .from("#Path_L_Page1", {drawSVG: "0%", duration: .5}, "logo_fill");
 
     return tl;
 }
