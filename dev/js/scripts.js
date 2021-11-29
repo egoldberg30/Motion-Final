79ba84a002ff57a0e0a2d57f0f84e467feeb4629
egoldberg30 <81780868+egoldberg30@users.noreply.github.com>
2021-11-28 21:00:48 -0500

    draw svg

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -6,17 +6,33 @@ import { CustomEase } from "gsap/CustomEase";
 gsap.registerPlugin(GSDevTools, MorphSVGPlugin, CustomEase);
 
 
+
+
 const mainTL = gsap.timeline();
 
 function LogoAnimation(){
     const tl = gsap.timeline();
 
-    // tl.from ("#book",{duration: 1, y:"-=1000", ease: CustomEase.create("custom", "M0,0 C0.136,0.078 0.382,0.263 0.578,0.448 0.78,0.638 0.932,0.802 1,1 ")});
-    tl.from ("#book",{duration: 1, y:"-=1000"});
+    tl.from ("#book",{duration: 1.3, y:"-=1000", ease: CustomEase.create("custom", "M0,0 C0.136,0.078 0.382,0.263 0.578,0.448 0.78,0.638 0.932,0.802 1,1 ")});
+    // tl.from ("#book",{duration: 1, y:"-=1000"});
     tl.to("#book", {morphSVG:"#book_squashed", duration: .25})
     .to("#book", {morphSVG:"#book", duration: .1})
     .to("#book_mask_1", {morphSVG:"#book_mask_2", duration: .01})
-    .to("#book",{duration: .5, y:"-=200"});
+    .to("#book",{duration: .5, y:"-=200"}, "logo_fill")
+
+    .to("#Path_R_Page6", {drawSVG: "100%", duration: .5}, "logo_fill")
+    .to("#Path_R_Page5", {drawSVG: "100%", duration: .5}, "logo_fill")
+    .to("#Path_R_Page4", {drawSVG: "100%", duration: .5}, "logo_fill")
+    .to("#Path_R_Page3", {drawSVG: "100%", duration: .5}, "logo_fill")
+    .to("#Path_R_Page2", {drawSVG: "100%", duration: .5}, "logo_fill")
+    .to("#Path_R_Page1", {drawSVG: "100%", duration: .5}, "logo_fill")
+
+    .to("#Path_L_Page6", {drawSVG: "100%", duration: .5}, "logo_fill")
+    .to("#Path_L_Page5", {drawSVG: "100%", duration: .5}, "logo_fill")
+    .to("#Path_L_Page4", {drawSVG: "100%", duration: .5}, "logo_fill")
+    .to("#Path_L_Page3", {drawSVG: "100%", duration: .5}, "logo_fill")
+    .to("#Path_L_Page2", {drawSVG: "100%", duration: .5}, "logo_fill")
+    .to("#Path_L_Page1", {drawSVG: "100%", duration: .5}, "logo_fill");
 
     return tl;
 }
