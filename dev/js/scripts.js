1583c65d4713afce599fcd48c6deae290468f57b
egoldberg30 <81780868+egoldberg30@users.noreply.github.com>
2021-12-08 12:36:53 -0500

    tweaks

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -39,6 +39,22 @@ function LogoAnimation(){
     return tl;
 }
 
+
+tl.from ("#book",{duration: 1.3, y:"-=1000", ease: CustomEase.create("custom", "M0,0 C0.136,0.078 0.382,0.263 0.578,0.448 0.78,0.638 0.932,0.802 1,1 ")});
+
+customease bounce
+
+paired with customease squash or scalex
+
+
+
+
+
+
+
+
+
+
 mainTL.add(LogoAnimation());
 
 GSDevTools.create();
