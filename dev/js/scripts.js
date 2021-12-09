58fad8943217253bb48d077e922c60f88db97b6a
egoldberg30 <81780868+egoldberg30@users.noreply.github.com>
2021-12-08 19:09:24 -0500

    full sequence

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -8,7 +8,6 @@ gsap.registerPlugin(GSDevTools, MorphSVGPlugin, CustomEase, DrawSVGPlugin);
 
 
 
-
 const mainTL = gsap.timeline();
 
 function LogoAnimation(){
@@ -37,21 +36,20 @@ function LogoAnimation(){
     tl.to("#logo_spreadout_solid", {autoAlpha:1, duration: 0.01}, "vectorswitcharoo")
     .to("#logo_spreadout_masks", {autoAlpha:0, duration: 0.01}, "vectorswitcharoo");
 
-    tl.from("#type_1_with_typecover", {duration: 0.7, autoAlpha:0, y:"-=20", ease: "none"}, "pagessettle");
-    tl.to("#Solid_R_Page6", {morphSVG:"#R_Page6_2", duration: 0.9}, "pagessettle")
-    .to("#Solid_R_Page5", {morphSVG:"#R_Page5_2", duration: 0.9}, "pagessettle")
-    .to("#Solid_R_Page4", {morphSVG:"#R_Page4_2", duration: 0.9}, "pagessettle")
-    .to("#Solid_R_Page3", {morphSVG:"#R_Page3_2", duration: 0.9}, "pagessettle")
-    .to("#Solid_R_Page2", {morphSVG:"#R_Page2_2", duration: 0.9}, "pagessettle")
-    .to("#Solid_R_Page1", {morphSVG:"#R_Page1_2", duration: 0.9}, "pagessettle")
-    .to("#Solid_L_Page6", {morphSVG:"#L_Page6_2", duration: 0.9}, "pagessettle")
-    .to("#Solid_L_Page5", {morphSVG:"#L_Page5_2", duration: 0.9}, "pagessettle")
-    .to("#Solid_L_Page4", {morphSVG:"#L_Page4_2", duration: 0.9}, "pagessettle")
-    .to("#Solid_L_Page3", {morphSVG:"#L_Page3_2", duration: 0.9}, "pagessettle")
-    .to("#Solid_L_Page2", {morphSVG:"#L_Page2_2", duration: 0.9}, "pagessettle")
-    .to("#Solid_L_Page1", {morphSVG:"#L_Page1_2", duration: 0.9}, "pagessettle");
+    tl.from("#type_1_with_typecover", {duration: 1.1, autoAlpha:0, y:"+=15"}, "pagessettle");
+    tl.to("#Solid_R_Page6", {morphSVG:"#R_Page6", duration: 1.1}, "pagessettle")
+    .to("#Solid_R_Page5", {morphSVG:"#R_Page5", duration: 1.1}, "pagessettle")
+    .to("#Solid_R_Page4", {morphSVG:"#R_Page4", duration: 1.1}, "pagessettle")
+    .to("#Solid_R_Page3", {morphSVG:"#R_Page3", duration: 1.1}, "pagessettle")
+    .to("#Solid_R_Page2", {morphSVG:"#R_Page2", duration: 1.1}, "pagessettle")
+    .to("#Solid_R_Page1", {morphSVG:"#R_Page1", duration: 1.1}, "pagessettle")
+    .to("#Solid_L_Page6", {morphSVG:"#L_Page6", duration: 1.1}, "pagessettle")
+    .to("#Solid_L_Page5", {morphSVG:"#L_Page5", duration: 1.1}, "pagessettle")
+    .to("#Solid_L_Page4", {morphSVG:"#L_Page4", duration: 1.1}, "pagessettle")
+    .to("#Solid_L_Page3", {morphSVG:"#L_Page3", duration: 1.1}, "pagessettle")
+    .to("#Solid_L_Page2", {morphSVG:"#L_Page2", duration: 1.1}, "pagessettle")
+    .to("#Solid_L_Page1", {morphSVG:"#L_Page1", duration: 1.1}, "pagessettle")
 
-    tl.from ("#type_1_with_typecover", {duration: 1.3, autoAlpha:0, y:"-=100"})
     .to("#typecover_0C, #typecover_0D, #typecover_0E, #typecover_0F, #typecover_0G, #typecover_1A, #typecover_1B, #typecover_1C, #typecover_1D, #typecover_1E", {autoAlpha:1, duration:0.01}, "simultaneous1")
     .to("#typecover_0A", {autoAlpha:0, duration:0.01}, "simultaneous1")
 
@@ -73,6 +71,7 @@ function LogoAnimation(){
 
     .to("#type_1", {autoAlpha:0, duration:0.01}, "simultaneous2")
     .to("#type_2", {autoAlpha:1, duration:0.01}, "simultaneous2")
+    .to("#typecover_1A, #typecover_1B, #typecover_1C, #typecover_1D", {autoAlpha:0, duration:0.01}, "simultaneous2")
     .to("#typecover_2A, #typecover_2B, #typecover_2C, #typecover_2D, #typecover_2E, #typecover_2F, #typecover_2G", {autoAlpha:1, duration:0.01}, "simultaneous2")
 
     .to("#typecover_2A", {autoAlpha:0, duration:0.01}, "+=.1")
@@ -83,7 +82,6 @@ function LogoAnimation(){
     .to("#typecover_2F", {autoAlpha:0, duration:0.01}, "+=.1")
     .to("#typecover_2G", {autoAlpha:0, duration:0.01}, "+=.1")
 
-    .to("#typecover_1F", {autoAlpha:1, duration:0.01}, "+=.07")
     .to("#typecover_1E", {autoAlpha:1, duration:0.01}, "+=.07")
     .to("#typecover_1D", {autoAlpha:1, duration:0.01}, "+=.07")
     .to("#typecover_1C", {autoAlpha:1, duration:0.01}, "+=.07")
@@ -93,6 +91,7 @@ function LogoAnimation(){
     .to("#type_2", {autoAlpha:0, duration:0.01}, "simultaneous3")
     .to("#type_3", {autoAlpha:1, duration:0.01}, "simultaneous3")
     .to("#typecover_2A, #typecover_2B, #typecover_2C, #typecover_2D, #typecover_2E, #typecover_2F, #typecover_2G", {autoAlpha:0, duration:0.01}, "simultaneous3")
+    .to("#typecover_1A, #typecover_1B, #typecover_1C, #typecover_1D, #typecover_1E", {autoAlpha:0, duration:0.01}, "simultaneous3")
     .to("#typecover_3A, #typecover_3B, #typecover_3C, #typecover_3D, #typecover_3E", {autoAlpha:1, duration:0.01}, "simultaneous3")
 
     .to("#typecover_3A", {autoAlpha:0, duration:0.01}, "+=.1")
